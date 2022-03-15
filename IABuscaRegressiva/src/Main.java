8c67dd520b0df18a1a99935dffdd54728275c644
Davi Ferreira <daviferreirart@gmail.com>
2022-03-15 19:21:16 -0300

    ligando pontos

--- IABuscaRegressiva/src/Main.java
+++ IABuscaRegressiva/src/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
 		Grafo grafo = new Grafo(12);
 
-		// [0,1,2,3,4,5,6,7,8,9]
-		// [A,B,C,D,E,F,G,H,I,J]
+		// [0,1,2,3,4,5,6,7,8,9,10,11]
+		// [A,B,C,D,E,F,G,H,I,J,K]
 
 		grafo.addAresta(0, 1);
 		grafo.addAresta(0, 2);
@@ -22,20 +22,23 @@ public class Main {
 		grafo.addAresta(4, 7);
 		grafo.addAresta(4, 8);
 
-		grafo.addAresta(5, 11);
+		grafo.addAresta(5, 9);
 
 		int origem = 0;
 		int destino = 6;
 
 		System.out.println("Caminho de A até G:");
 		BuscaRegressiva busca1 = new BuscaRegressiva(grafo, origem, destino);
-		
-		origem = 0;
+
 		destino = 10;
 		
-		System.out.println("Caminho de A até M:");
+		System.out.println("Caminho de A até K:");
 		BuscaRegressiva busca2 = new BuscaRegressiva(grafo, origem, destino);
+		
+		grafo.addAresta(5, 10);
 
+		System.out.println("Caminho de A até K:");
+		busca2 = new BuscaRegressiva(grafo, origem, destino);
 	}
 
 }
